5cda7752eff400fa27b4e4f5f73e2a1b9b74478f
Cyanflxy <cyanflxy@163.com>
2018-03-07 16:19:32 +0800

    55. Jump Game Java solution

--- LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_55_Jump_Game.java
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_55_Jump_Game.java
@@ -26,57 +26,49 @@ public class _55_Jump_Game {
     private static void test() {
         _55_Jump_Game object = new _55_Jump_Game();
 
-//        System.out.println(object.canJump(new int[]{2, 3, 1, 1, 4}));//true
-//        System.out.println(object.canJump(new int[]{3, 2, 1, 0, 4}));//false
-//        System.out.println(object.canJump(new int[]{1, 2}));//true
-//        System.out.println(object.canJump(new int[]{4, 3, 2, 1, 0, 0}));//false
+        System.out.println(object.canJump(new int[]{2, 3, 1, 1, 4}));//true
+        System.out.println(object.canJump(new int[]{3, 2, 1, 0, 4}));//false
+        System.out.println(object.canJump(new int[]{1, 2}));//true
+        System.out.println(object.canJump(new int[]{4, 3, 2, 1, 0, 0}));//false
         System.out.println(object.canJump(new int[]{1, 2, 3}));//true
-        System.out.println(object.canJump(new int[]{0}));//false
+        System.out.println(object.canJump(new int[]{0}));//true
     }
 
-
-    public boolean canJump(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return false;
-        }
-
-        int p = nums.length - 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] + i >= p) {
-                p = i;
-            }
-        }
-
-        return p == 0;
-    }
 
 //    public boolean canJump(int[] nums) {
 //        if (nums == null || nums.length == 0) {
 //            return false;
 //        }
 //
-//        int len = nums.length;
-//        boolean[] possible = new boolean[len];
-//        Arrays.fill(possible, false);
-//        possible[0] = true;
-//        int maxPossible = 0;
-//
-//        for (int i = 0; i < len - 1; i++) {
-//            if (possible[i]) {
-//
-//                if (i + nums[i] > maxPossible) {
-//                    if (i + nums[i] >= len - 1) {
-//                        return true;
-//                    }
-//                    for (int j = maxPossible + 1; j <= i + nums[i] && j < len; j++) {
-//                        possible[j] = true;
-//                    }
-//                    maxPossible = i + nums[i];
-//                }
-//
+//        int p = nums.length - 1;
+//        for (int i = nums.length - 2; i >= 0; i--) {
+//            if (nums[i] + i >= p) {
+//                p = i;
 //            }
 //        }
 //
-//        return possible[len - 1];
+//        return p == 0;
 //    }
+
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int len = nums.length;
+        int max = 0;
+
+        for (int i = 0; i < len - 1; i++) {
+            if (max >= i) {
+                max = Math.max(max, i + nums[i]);
+                if (max >= len - 1) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return max >= len - 1;
+    }
 }
