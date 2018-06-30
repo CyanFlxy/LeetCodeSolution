fa80c5a644bf3783076c0c2d96c07a8182549f4d
Cyanflxy <cyanflxy@163.com>
2018-06-30 21:28:03 +0800

    TreeNode创建方法

--- LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/help/TreeNode.java
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/help/TreeNode.java
@@ -25,4 +25,50 @@ public class TreeNode {
 
         return cur + left + " " + right;
     }
+
+    public static TreeNode create(Integer... values) {
+        if (values == null || values.length == 0) {
+            return null;
+        }
+        int len = values.length;
+        TreeNode[] nodeArray = new TreeNode[len];
+        for (int i = 0; i < len; i++) {
+            if (values[i] != null) {
+                nodeArray[i] = new TreeNode(values[i]);
+            }
+        }
+
+        int level = 0;
+        int index = 0;
+
+        outer:
+        do {
+            int currentLevelEnd = index + (1 << level);
+            int nextIndex = currentLevelEnd;
+
+            for (int i = index; i < currentLevelEnd && i < len && nodeArray[i] != null; i++) {
+                if (nextIndex >= len) {
+                    break outer;
+                }
+                nodeArray[i].left = nodeArray[nextIndex];
+
+                if (nextIndex + 1 >= len) {
+                    break outer;
+                }
+                nodeArray[i].right = nodeArray[nextIndex + 1];
+
+                nextIndex += 2;
+            }
+            index = currentLevelEnd;
+            level++;
+        } while (index < len);
+
+        return nodeArray[0];
+    }
+
+
+    public static void main(String... args) {
+        TreeNode node = create(1, 2, 3, 4, 5, 6, null, 8, 9);
+        System.out.println(node);
+    }
 }
