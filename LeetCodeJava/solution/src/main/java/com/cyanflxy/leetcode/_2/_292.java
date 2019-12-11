ec6e7fc7ffa5419d1fdca295c55e134956027585
Cyanflxy <cyanflxy@163.com>
2019-12-11 23:33:49 +0800

    292. Nim 游戏

---
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_2/_292.java
@@ -0,0 +1,11 @@
+package com.cyanflxy.leetcode._2;
+
+/**
+ * @author cyanflxy
+ * @since 2019/12/11
+ */
+public class _292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
