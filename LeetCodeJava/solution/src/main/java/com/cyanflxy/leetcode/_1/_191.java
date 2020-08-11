b0493b224b1eae079a50cb960a3b1b0889a46a7c
Cyanflxy <cyanflxy@163.com>
2020-08-11 21:52:20 +0800

    191. 位1的个数

---
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_1/_191.java
@@ -0,0 +1,12 @@
+package com.cyanflxy.leetcode._1;
+
+/**
+ * @author cyanflxy
+ * @since 2020/8/11
+ */
+public class _191 {
+
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
