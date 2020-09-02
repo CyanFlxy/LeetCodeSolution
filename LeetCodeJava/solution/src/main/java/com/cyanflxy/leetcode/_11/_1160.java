30a6b4383d6b5b0eda4b07d146b9a8752097244d
Cyanflxy <cyanflxy@163.com>
2020-09-03 00:30:46 +0800

    1160. 拼写单词

--- LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_11/_1160.java
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_11/_1160.java
@@ -7,13 +7,16 @@ import java.util.Arrays;
  * @since 2020/9/3
  */
 public class _1160 {
+
     public int countCharacters(String[] words, String chars) {
-        char[] dic = chars.toCharArray();
-        Arrays.sort(dic);
+        int[] dic = new int[26];
+        for (int i = 0; i < chars.length(); i++) {
+            dic[chars.charAt(i) - 'a']++;
+        }
 
         int result = 0;
         for (String s : words) {
-            if (isContain(dic, s)) {
+            if (contain(s, dic)) {
                 result += s.length();
             }
         }
@@ -21,7 +24,34 @@ public class _1160 {
         return result;
     }
 
-    private boolean isContain(char[] src, String destStr) {
+    private boolean contain(String str, int[] dic) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            int index = str.charAt(i) - 'a';
+            count[index]++;
+            if (count[index] > dic[index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public int countCharacters1(String[] words, String chars) {
+        char[] dic = chars.toCharArray();
+        Arrays.sort(dic);
+
+        int result = 0;
+        for (String s : words) {
+            if (isContain1(dic, s)) {
+                result += s.length();
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isContain1(char[] src, String destStr) {
         char[] dst = destStr.toCharArray();
         Arrays.sort(dst);
 
