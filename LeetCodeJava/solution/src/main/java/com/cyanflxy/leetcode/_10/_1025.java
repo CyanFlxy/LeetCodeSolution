d0a719257493036ea220ef9bbdc110766b54d39a
Cyanflxy <cyanflxy@163.com>
2020-09-08 22:22:20 +0800

    1025. 除数博弈

---
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_10/_1025.java
@@ -0,0 +1,11 @@
+package com.cyanflxy.leetcode._10;
+
+/**
+ * @author cyanflxy
+ * @since 2020/9/8
+ */
+public class _1025 {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
