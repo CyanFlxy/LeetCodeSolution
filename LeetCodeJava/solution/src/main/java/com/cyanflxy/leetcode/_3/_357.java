2f5d04f796846546587356ee97acd600039e66b4
Cyanflxy <cyanflxy@163.com>
2020-11-05 23:44:00 +0800

    357. 计算各个位数不同的数字个数

---
+++ LeetCodeJava/solution/src/main/java/com/cyanflxy/leetcode/_3/_357.java
@@ -0,0 +1,12 @@
+package com.cyanflxy.leetcode._3;
+
+/**
+ * @author cyanflxy
+ * @since 2020/11/5
+ */
+public class _357 {
+    public int countNumbersWithUniqueDigits(int n) {
+        int[] result = {1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851, 5611771, 8877691};
+        return result[n];
+    }
+}
