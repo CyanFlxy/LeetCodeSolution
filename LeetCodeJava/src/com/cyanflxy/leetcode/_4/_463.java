c7b7b4b874716fad6829607080ca8402857e842c
Cyanflxy <cyanflxy@163.com>
2020-11-13 22:52:48 +0800

    463. 岛屿的周长

---
+++ LeetCodeJava/src/com/cyanflxy/leetcode/_4/_463.java
@@ -0,0 +1,23 @@
+package com.cyanflxy.leetcode._4;
+
+public class _463 {
+    public int islandPerimeter(int[][] grid) {
+        int result = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    result += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        result -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        result -= 2;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
