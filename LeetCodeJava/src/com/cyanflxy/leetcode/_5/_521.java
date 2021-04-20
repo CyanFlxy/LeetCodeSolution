7b4024ed479db1315c385984cc6a98d534de9a31
Cyanflxy <cyanflxy@163.com>
2021-04-20 23:52:44 +0800

    521. 最长特殊序列 Ⅰ

---
+++ LeetCodeJava/src/com/cyanflxy/leetcode/_5/_521.java
@@ -0,0 +1,10 @@
+package com.cyanflxy.leetcode._5;
+
+public class _521 {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+    }
+}
