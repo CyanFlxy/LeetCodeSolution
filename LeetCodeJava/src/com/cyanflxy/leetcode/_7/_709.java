d409346ce565b45cd23d76e86817fe7334d3b516
Cyanflxy <cyanflxy@163.com>
2021-05-06 17:24:52 +0800

    709. 转换成小写字母

---
+++ LeetCodeJava/src/com/cyanflxy/leetcode/_7/_709.java
@@ -0,0 +1,11 @@
+package com.cyanflxy.leetcode._7;
+
+/**
+ * @author cyanflxy
+ * @since 2021.05.06
+ */
+public class _709 {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
