e9819b7ad5ad434acfac29d431e5d741b7d540f9
Cyanflxy <cyanflxy@163.com>
2021-06-23 22:15:11 +0800

    剑指 Offer 15. 二进制中1的个数

---
+++ LeetCodeJava/src/com/cyanflxy/leetcode/offer/Offer15.java
@@ -0,0 +1,11 @@
+package com.cyanflxy.leetcode.offer;
+
+/**
+ * @author cyanflxy
+ * @since 2021.06.23
+ */
+public class Offer15 {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
