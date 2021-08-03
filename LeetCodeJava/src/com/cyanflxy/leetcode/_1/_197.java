2c9ba450c7e83cf7d7c595dc3cc50a56e486bbcc
Cyanflxy <cyanflxy@163.com>
2021-08-03 23:15:19 +0800

    197. 上升的温度

---
+++ LeetCodeJava/src/com/cyanflxy/leetcode/_1/_197.java
@@ -0,0 +1,11 @@
+package com.cyanflxy.leetcode._1;
+
+/**
+ * @author cyanflxy
+ * @since 2021.08.03
+ */
+public class _197 {
+//    MySQL
+//    select w2.id from Weather w1, Weather w2
+//    where datediff(w2.recordDate, w1.recordDate) = 1 and w1.Temperature < w2.Temperature
+}
