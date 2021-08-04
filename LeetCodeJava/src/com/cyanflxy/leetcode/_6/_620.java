7c7c0fa8054ff55b455ff0c4bb15e86665a24cca
Cyanflxy <cyanflxy@163.com>
2021-08-04 23:08:12 +0800

    620. 有趣的电影

---
+++ LeetCodeJava/src/com/cyanflxy/leetcode/_6/_620.java
@@ -0,0 +1,12 @@
+package com.cyanflxy.leetcode._6;
+
+/**
+ * @author cyanflxy
+ * @since 2021.08.04
+ */
+public class _620 {
+//    # Write your MySQL query statement below
+//    select * from cinema
+//    where id % 2 = 1 and description != 'boring'
+//    order by rating desc
+}
